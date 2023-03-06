4ee14b7288410c4731ade5417ae1f5c941cc0ef7
ffekirnew <ffekirnew0808@gmail.com>
2023-03-06 23:03:43 +0300

    finished curating the data into three columns

---
+++ main.py
@@ -0,0 +1,27 @@
+import csv
+
+all_data = []
+
+def curate_data(store_array, csv_reader):
+    for row in csv_reader:
+        if row[0] == "pink morsel":
+            price = float(row[1].replace("$", ""))
+            amount = int(row[2])
+            date = row[3]
+            region = row[4]
+
+            sales = price * amount
+            store_array.append([sales, date, region])
+
+for i in range(3):
+    with open(f'./data/daily_sales_data_{i}.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+
+        curate_data(all_data, csv_reader)
+
+with open(file='./data/curated_data.csv', mode='w') as csv_file:
+    csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    csv_writer.writerows(all_data)
+
+
