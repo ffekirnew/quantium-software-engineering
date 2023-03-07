8e5415834b4c6f4d7ce9a2f809aa975d20140bf7
ffekirnew <ffekirnew0808@gmail.com>
2023-03-07 16:16:19 +0300

    completed task 4

--- pink_moresel_sale_visualizer.py
+++ pink_moresel_sale_visualizer.py
@@ -1,7 +1,7 @@
 # Run this app with `python pink_moresel_sale_visualizer.py` and
 # visit http://127.0.0.1:8050/ in your web browser.
 
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -11,15 +11,51 @@ app = Dash(__name__)
 
 df = pd.read_csv(CSV_FILE)
 
-line_graph = px.line(df, x='date', y='sales', color='region', hover_name='sales')
+regions = ['all', 'north', 'south', 'east', 'west']
+
+figures = {}
+for region in regions:
+    if region == 'all':
+        region_df = df
+    else:
+        region_df = df.query(f'region=="{region}"')
+    figures[region] = px.line(region_df, x='date', y='sales', color='region', hover_name='sales')
 
 app.layout = html.Div(children=[
-    html.H1(children="Soul Food's Pink Morsel Sale Visualization"),
+    html.H1(
+        children="Soul Food's Pink Morsel Sale Visualization",
+        style={
+            "fontSize" : 32,
+            "fontFamily": "Arial",
+            "textAlign": 'center'
+        }),
+    dcc.RadioItems(
+        id='region-radio',
+        options=[{'label': region.capitalize(), 'value': region} for region in regions],
+        value='all',
+        labelStyle={
+            'padding': '2px',
+            'display': 'inline-block',
+            'margin-right': '10px',
+            'align': 'center'
+        }
+    ),
     dcc.Graph(
-        id='soul-food-change-outcomes',
-        figure=line_graph
-    )
-])
+        id='soul-food-change-outcomes'
+    ),
+], style={
+    'fontFamily': 'Arial',
+    'textAlign': 'center',
+})
+
+
+@app.callback(
+    Output(component_id='soul-food-change-outcomes', component_property='figure'),
+    Input(component_id='region-radio', component_property='value')
+)
+def update_output_figure(region_name):
+    return figures[region_name]
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
